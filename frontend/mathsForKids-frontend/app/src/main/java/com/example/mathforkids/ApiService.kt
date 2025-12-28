ade1703248da2e5e932a70ceb62cbb933814804a
Vuolzsupe <phandinhnam538@gmail.com>
2025-12-28 21:06:48 +0700

    Update APPv2

--- frontend/mathsForKids-frontend/app/src/main/java/com/example/mathforkids/ApiService.kt
+++ frontend/mathsForKids-frontend/app/src/main/java/com/example/mathforkids/ApiService.kt
@@ -75,7 +75,7 @@ interface ApiService {
         //private const val BASE_URL = "http://10.0.2.2:8000/"
     //}
         // Thay đổi IP này theo địa chỉ máy tính chạy Server của bạn
-        private const val BASE_URL = " https://share-cook-fraser-spin.trycloudflare.com "
+        private const val BASE_URL = " https://subscriber-exotic-paid-husband.trycloudflare.com "
 
        fun create(): ApiService {
             return Retrofit.Builder()
